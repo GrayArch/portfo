68e212460daa392584e7fed814d591b4d394c4ae
RedTeamerGray <muriithidennis340@gmail.com>
2023-12-27 02:05:17 +0300

    code update

--- pages/api/background.js
+++ pages/api/background.js
@@ -3,24 +3,24 @@ const background = [
         eduCards: [
             {
                 id: 0,
-                title: 'UET Peshawar',
-                degree: 'BS, Computer System Engineering',
-                detail: "Bachelor's Degree in Computer System Engineering from UET Peshawar.",
+                title: 'Karatina University',
+                degree: 'BSc, Information Technology',
+                detail: "Bachelor's Degree in Information Technology.",
                 year: '2018-2022'
             },
             {
                 id: 1,
-                title: 'Peshawar Model Degree College',
-                degree: 'HSSC, Pre Engineering',
-                detail: "Completed FSC part 1 and part 2 in Pre-Engineering from Peshawar Model Degree College.",
-                year: '2015-2017'
+                title: 'Kanyakine High School',
+                degree: 'KCSE',
+                detail: "Completed Kenya certificate of Secondary education.",
+                year: '2016-2021'
             },
             {
                 id: 2,
-                title: 'Falcon Academy',
-                degree: 'SSC, Science Subjects',
-                detail: "Completed SSC part 1 and part 2 in Science subjects from Falcon Academy Seni Gumbat Kohat",
-                year: '2013-2015'
+                title: 'Anne`s Junior Academy',
+                degree: 'KCPE',
+                detail: "Completed Kenya certificate of primary education.",
+                year: '2006-2016'
             },
         ]
     },
@@ -28,12 +28,12 @@ const background = [
         expCards: [
             {
                 id: 1,
-                title: 'JMM Technologies',
-                role: 'Frontend Developer',
-                url: 'https://jmm.ltd/',
-                desc: 'As a frontend developer, I use React, Next & JavaScript to build user interfaces for web applications.',
-                year: '02/2023 - Present',
-                location: 'Peshawar, Pakistan'
+                title: 'ONIX-MARKET',
+                role: 'Penetration Tester',
+                url: 'https://onix-market.com/',
+                desc: 'As a PenTester, I test the Systems To Discover Vulnerabilities And help th company Secure their Cyber Space.',
+                year: '09/2023 - Present',
+                location: 'Nairobi, Kenya'
             },
             {
                 id: 2,
@@ -42,7 +42,7 @@ const background = [
                 url: 'no website',
                 desc: 'As an Internee, I learned how to use React & JavaScript to build interactive websites.',
                 year: '02/2023 - Present',
-                location: 'Peshawar, Pakistan'
+                location: 'Nairobi, Kenya'
             },
             {
                 id: 3,
@@ -51,7 +51,7 @@ const background = [
                 url: 'https://www.encoderbytes.com/',
                 desc: "I work there as a PHP developer, there I learned how to do CRUD'S operations in PHP, also I worked on Firebase",
                 year: '09/2020 - 02/2021',
-                location: 'Peshawar, Pakistan'
+                location: 'Nairobi, Kenya'
             },
 
         ]
