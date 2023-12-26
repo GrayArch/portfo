62d53a90ab96ff78f64d0ed0109586311cf25b7a
RedTeamerGray <muriithidennis340@gmail.com>
2023-12-26 23:53:59 +0300

    code commit

---
+++ constants/constants.js
@@ -0,0 +1,64 @@
+export const NAME = 'Muriithi Dennis';
+
+export const DESIGNATION = `MERN Stack Developer | Open Source Contributor | PenTester`;
+
+export const DETAILS = {
+  Residence: 'Kenya',
+  City: 'Nairobi',
+  Age: '21',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '75%',
+  },
+  {
+    title: 'React Developer',
+    level: '91%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Backend Developer',
+    level: '54%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '83%',
+  },
+];
+
+export const TECH_STACK = [
+  'ReactJS',
+  'JavaScript',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Figma',
+];
+
+export const CONTACTS = {
+  EMAIL: 'muriithidennis340@gmail.com',
+  PHONE: '+254112395869',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/RedTeamerGray',
+  LINKEDIN: 'https://www.linkedin.com/in/redteamergray/',
+  TWITTER: 'https://twitter.com/14payload',
+  FACEBOOK: 'https://www.facebook.com/14payload',
+};
